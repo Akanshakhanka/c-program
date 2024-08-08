1dc17ae90cd6d8900289e5a72800f05e7eb38e85
Akanshakhanka <133778793+Akanshakhanka@users.noreply.github.com>
2024-08-08 11:44:53 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,42 @@
+//let c=150;
+//let d=200;
+//let e="gic road"
+//let f="Akansha"
+//let g=true
+//let h=undefined
+//console.log(typeof(h))//
+//let a=50;
+//let b=100;//
+//console.log(a<b);//
+//console.log(a==b);//
+//arithmatic +-*/%**++--
+//relational operator <,>,<=,>=,!=,==,===,!==
+//logical operator && || not
+//assignment operation +=,-=,*=,/=,,%=
+//let a=50;
+//let b=100;
+//let c=150;
+//console.log(!(a<b && b<c))
+let a=10;
+a+=5
+a-=2
+a%=2
+//console.log(a)
+let x="aarti";
+let y="pal ji";
+console.log(x+y)
+//conditional statement
+let age=17;
+if (age>18){
+    console.log("please aap vote kariye")
+}else if (age==18){
+    console.log("We will think over it")
+}else{
+    console.log("bhag ja yahan se");
+}
+let day;
+if(day==1){
+    console.log("Monday");
+}
+
+
